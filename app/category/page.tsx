4e482fd399aad0585c86a1416454e6cfc13a3a86
firzhaardhia <firzharamadhan27@gmail.com>
2024-10-26 20:44:12 +0700

    remove use server

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,4 +1,3 @@
-"use server";
 import { createClient } from "@/utils/supabase/server"
 import React from 'react'
 import { Button } from "@/components/ui/button"
--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import React from 'react'
 import { createClient } from '@/utils/supabase/server';
 import Layout from '@/components/Layout'
